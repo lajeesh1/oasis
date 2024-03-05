114a04d62cd5c996d5a265290fe7d6100a145956
Lajeesh <gokulam.9920@gmail.com>
2024-03-05 16:49:59 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  color: red;
+  font-size: 25px;
+`;
+
+const Button = styled.button`
+  background-color: green;
+  color: white;
+`;
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <H1>hello</H1>
+      <Button>OK</Button>
+    </div>
+  );
 }
 
 export default App;
