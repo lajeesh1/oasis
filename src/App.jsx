dbfa4598c5407ba5ff2a3f22d18d1911f4679422
Lajeesh <gokulam.9920@gmail.com>
2024-03-09 09:30:05 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
 import styled from "styled-components";
 
 const H1 = styled.h1`
-  color: red;
-  font-size: 25px;
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
 `;
 
 const Button = styled.button`
-  background-color: green;
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
   color: white;
+  margin: 10px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: lightgray;
+  padding: 20px;
 `;
 
 function App() {
   return (
-    <div>
-      <H1>hello</H1>
-      <Button>OK</Button>
-    </div>
+    <StyledApp>
+      <H1>The wild oasis</H1>
+      <Button>Check-in</Button>
+      <Button>Check-out</Button>
+      <Input maxLength={2} type="number" placeholder="Number of gust" />
+    </StyledApp>
   );
 }
 
